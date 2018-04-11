c8c4cdc399634751e3bb190d7a63d54d01bb881e
Ezequiel Salas <ezjava@gmail.com>
2018-04-10 23:46:21 -0400

    list of product

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+
+
+
+
 const tempImage = "https://d3d71ba2asa5oz.cloudfront.net/12015576/images/iphone%207%20plus%20gold%20generic%20front_41117.jpg";
 
 const Article =(props) =>{
@@ -17,10 +22,48 @@ const Article =(props) =>{
   );
 }
 
+const ShopingCar =(props) =>{
+  return(
+  <div className='shopping-panel'>
+  
+  <h1>I'm a shoppingCar</h1>
+  </div>
+  );
+}
+
+
+const ArticleList = (props) =>{
+  return (
+    <div className="container-list">
+
+    {props.articles.map(article => (
+           <Article name="SmartPhone" price="80.20" img={tempImage}/> 
+        ))}
+     
+    </div>
+    );
+}
 class App extends Component{
+  constructor(props) {
+    super(props);
+    this.state = { items: [], text: '' };
+    this.articles = [1,2,3,4];
+  }
 
   render(){
-    return (<Article name="SmartPhone" price="80.20" img={tempImage}/>);
+    return (
+     <div>
+       <ul className="navigation">
+  
+        <li><a href="#">Products</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+      <div className="container">
+      <ArticleList articles={this.articles} />
+      <ShopingCar />
+      </div>
+     </div>
+      );
   }
 };
 
