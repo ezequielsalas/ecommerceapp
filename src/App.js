8f3022143048b4694c0527f885a15a0c666f4f6e
esalas <esalas@encorerepair.com>
2018-04-11 13:21:37 -0400

    Change to mockmodel

--- src/App.js
+++ src/App.js
@@ -3,12 +3,6 @@ import logo from './logo.svg';
 import './App.css';
 
 
-
-
-
-
-const tempImage = "https://d3d71ba2asa5oz.cloudfront.net/12015576/images/iphone%207%20plus%20gold%20generic%20front_41117.jpg";
-
 const Article =(props) =>{
   return(
   <div className='product-card'>
@@ -25,8 +19,27 @@ const Article =(props) =>{
 const ShopingCar =(props) =>{
   return(
   <div className='shopping-panel'>
-  
-  <h1>I'm a shoppingCar</h1>
+  <header>
+    I'm the header
+  </header>
+  <div className="article-selected">
+    <article>
+      1
+    </article>
+    
+    <article>
+      2
+    </article>
+    
+    <article>
+      3
+    </article>
+    
+  </div>
+
+  <footer>
+    I'm a footer
+  </footer>
   </div>
   );
 }
@@ -36,18 +49,21 @@ const ArticleList = (props) =>{
   return (
     <div className="container-list">
 
-    {props.articles.map(article => (
-           <Article name="SmartPhone" price="80.20" img={tempImage}/> 
-        ))}
+    {props.articles.map(article => (<Article name={article.name} price={article.price} img={article.img}/>)
+                        )}
      
     </div>
     );
 }
+
 class App extends Component{
   constructor(props) {
     super(props);
-    this.state = { items: [], text: '' };
-    this.articles = [1,2,3,4];
+    //this.state = { articles: [] };
+    this.articles = [{"name":"SmartPhone","price":700, "img":"https://d3d71ba2asa5oz.cloudfront.net/12015576/images/iphone%207%20plus%20gold%20generic%20front_41117.jpg"}
+    ,{"name":"Tesla Roadster","price":40000, "img":"https://hips.hearstapps.com/amv-prod-cad-assets.s3.amazonaws.com/media/assets/submodel/8616.jpg"}
+    ,{"name":"Laptop","price":3000, "img":"https://assets.pcmag.com/media/images/339392-apple-macbook-pro-15-inch-2013.jpg?width=1000&height=758"}
+    ];
   }
 
   render(){
